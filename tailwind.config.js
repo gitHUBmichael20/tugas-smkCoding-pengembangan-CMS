1ac9b5bc4ad3b4bd46dc63d0da938e0afc625d22
Michael Simbolon <carlosimbolon23@gmail.com>
2024-12-21 20:49:27 +0700

    commit project pertama

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./auth/login.html", "./content/landing-page.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
